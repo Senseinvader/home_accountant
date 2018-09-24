65e367e0a6d704371486f135f4f2b17b63ac2c2d
Senseinvader <d.ivantsov1983@gmail.com>
2018-09-24 22:07:58 +0200

    Create LoginController class extending AbstractHandler class to verify user at login stage

---
+++ src/main/java/com/codecool/krk/controllers/AbstractHandler.java
@@ -0,0 +1,92 @@
+package com.codecool.krk.controllers;
+
+import com.codecool.krk.model.SessionIdContainer;
+import com.sun.net.httpserver.HttpExchange;
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
+
+import java.io.*;
+import java.net.HttpCookie;
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class AbstractHandler {
+
+    private SessionIdContainer sessionIdContainer;
+
+    public AbstractHandler() {
+        sessionIdContainer = SessionIdContainer.getSessionIdContainer();
+    }
+
+    public SessionIdContainer getSessionIdContainer() {
+        return sessionIdContainer;
+    }
+
+    public String getSessionIDFromCookieStr(String cookieStr) {
+        HttpCookie cookie = HttpCookie.parse(cookieStr).get(0);
+        return cookie.toString().split("=")[1];
+
+    }
+
+    public boolean isLoggedIn(String sessionId) {
+        return sessionIdContainer.contains(sessionId);
+    }
+
+    public void sendResponse(HttpExchange httpExchange, String response) {
+        byte[] bytes = response.getBytes();
+        try {
+            httpExchange.sendResponseHeaders(200, response.length());
+            OutputStream os = httpExchange.getResponseBody();
+            os.write(bytes);
+            os.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendTemplateRespond(HttpExchange httpExchange, String templateName) {
+        JtwigTemplate template = JtwigTemplate.classpathTemplate(String.format("templates/%s.twig", templateName));
+        JtwigModel model = JtwigModel.newModel();
+        String response = template.render(model);
+        sendResponse(httpExchange, response);
+    }
+
+    public void redirectToLocation(HttpExchange httpExchange, String dest) throws IOException {
+        String hostPort = httpExchange.getRequestHeaders().get("host").get(0);
+        httpExchange.getResponseHeaders().set("Location", "http://" + hostPort + dest);
+        httpExchange.sendResponseHeaders(301, -1);
+    }
+
+    public Map<String, String> readFormData(HttpExchange exchange) {
+        String formData = "";
+
+        try {
+            InputStreamReader isr = new InputStreamReader(exchange.getRequestBody(), "utf-8");
+            BufferedReader br = new BufferedReader(isr);
+            formData = br.readLine();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return parseFormData(formData);
+    }
+
+    public Map<String, String> parseFormData(String formData) {
+        Map<String, String> inputs = new HashMap<>();
+        String key;
+        String value;
+        String[] pairs = formData.split("&");
+        for (String pair : pairs) {
+            String[] keyValue = pair.split("=");
+            key = keyValue[0];
+            try {
+                value = URLDecoder.decode(keyValue[1], "UTF-8");
+                inputs.put(key, value);
+
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+        return inputs;
+    }
+}
---
+++ src/main/java/com/codecool/krk/controllers/LoginController.java
@@ -0,0 +1,68 @@
+package com.codecool.krk.controllers;
+
+import com.codecool.krk.DAO.LoginDAO;
+import com.codecool.krk.model.SessionIdContainer;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.net.HttpCookie;
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.UUID;
+
+public class LoginController extends AbstractHandler implements HttpHandler {
+
+    private LoginDAO loginDAO = new LoginDAO();
+
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+
+        String method = httpExchange.getRequestMethod();
+        String cookieStr = httpExchange.getRequestHeaders().getFirst("Cookie");
+        HttpCookie cookie;
+        String sessionId;
+
+        if (cookieStr == null) {
+            sessionId = generateSessionID();
+            cookie = new HttpCookie("SessionID", sessionId);
+            httpExchange.getResponseHeaders().add("Set-Cookie", cookie.toString());
+        } else {
+            sessionId = getSessionIDFromCookieStr(cookieStr);
+        }
+        if (method.equals("GET") && !isLoggedIn(sessionId)) {
+            sendTemplateRespond(httpExchange, "login");
+        }
+        if (method.equals("GET") && isLoggedIn(cookieStr)) {
+            redirectToLocation(httpExchange, "/main");
+        }
+        if (method.equals("POST")) {
+            Map inputs = readFormData(httpExchange);
+            String login = (String) inputs.get("login");
+            String pass = (String) inputs.get("pass");
+            try {
+                if (validatePassword(login, pass)) {
+                    getSessionIdContainer().add(sessionId, login);
+                    redirectToLocation(httpExchange, "/main");
+                } else {
+                    redirectToLocation(httpExchange, "/login");
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    private String generateSessionID() {
+        return UUID.randomUUID().toString();
+    }
+
+    private boolean validatePassword(String login, String inputPass) throws SQLException {
+        String extractedPass = loginDAO.getPasswordByLogin(login);
+        if (!(extractedPass == null) && extractedPass.equals(inputPass)) {
+            return true;
+        }
+        return false;
+    }
+}
