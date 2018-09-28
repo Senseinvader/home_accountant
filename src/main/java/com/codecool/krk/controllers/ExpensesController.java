b5f735387c90dd58cb47ee39b904aa627cc03383
Senseinvader <d.ivantsov1983@gmail.com>
2018-09-28 13:47:15 +0200

    Create expenses page, create ExpensesController class (without handler method)

---
+++ src/main/java/com/codecool/krk/controllers/ExpensesController.java
@@ -0,0 +1,15 @@
+package com.codecool.krk.controllers;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+
+public class ExpensesController extends AbstractHandler implements HttpHandler {
+
+
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+
+    }
+}
